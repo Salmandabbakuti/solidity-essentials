9be97705f2707e09009729c13c0823b94c59d568
Dev Salman <dabbakutisalman@gmail.com>
2018-08-18 18:25:00 +0530

    Create Example.sol

---
+++ Example.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.24;
+contract Example {
+address owner;
+address[] learners;
+uint256 numberOfLearners;
+
+struct learner {
+string name;
+uint8 age;
+bytes32 occupation;
+uint256 ContactNumber;
+}
+mapping (address => learner)learnerInfo;
+constructor() public {
+owner = msg.sender
+};
+
+modifier onlyOwner {
+require(msg.sender==owner);
+_;
+
+function join() public payable {
+require (msg.value= 0.1 ether);
+learners.push(msg.sender);
+numberOfLearners++;
+now;
+
+function kill() public {
+if (msg.sender==owner)
+selfdestruct(owner);
+}
+function showOwners() public returns (uint) {
+return owner;
+}
