555eb115b4fb0ef78fa4ce9826b9776788c66f36
Dev Salman <dabbakutisalman@gmail.com>
2018-08-23 10:56:42 +0530

    Create Counter.sol

---
+++ Counter.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.24;
+
+contract counter {
+    uint256 counter;
+
+function increment() public{
+    counter++;
+    
+      } 
+
+function decrement() public{
+        counter--;
+        
+       }
+ function getcounter() public constant returns(uint256){
+    
+     return counter;
+ 
+       }
+ }
